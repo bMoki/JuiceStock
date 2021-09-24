c6be19c9f813501a1886f2380041cfd7875f8fa8
bMoki <breno26sonda@gmail.com>
2021-09-23 21:46:04 -0300

    Projeto

---
+++ src/test/java/com/TestProject/JuiceStock/JuiceStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TestProject.JuiceStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuiceStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
